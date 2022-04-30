379aacc4f04df459a8fb76b392bc2e2d4df1d942
Matt <mattp303@gmail.com>
2022-04-30 17:25:52 -0600

    Added prompts for manager and add user

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const engineer = require('./lib/engineerClass');
 const intern = require('./lib/internClass');
 
 // Generate HTML from objects in team array
-const generateHTML = require('./src/generateHTML')
+const generateHTML = require('./src/generateHTML');
+const Choices = require('inquirer/lib/objects/choices');
 
 // Employee Array, objects created by function
 const teamArray = [];
@@ -19,6 +20,7 @@ function start() {
 }
 
 // Gather user input via terminal inquirer prompts
+// Start with manager information per acceptance criteria
 function managerInput() {
     inquirer
         .prompt([
@@ -43,4 +45,39 @@ function managerInput() {
                 message: 'Please enter the managers office number:'
             },
         ])
+        .then((val) => {
+            const manager = new Manager(
+                val.name,
+                val.employeeID,
+                val.employeeEmail,
+                val.officeNumber
+            );
+            console.table(Manager);
+            teamArray.push(Manager);
+            addMember();
+        });
 }
+
+// Prompt user if they would like to add a team member, if yes, what type
+function addMember() {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'employeeType',
+            message: 'Would you like to add another team member?',
+            choices: ['Engineer', 'Intern', 'Done for now!'],
+        },
+    ])
+    .then((val) => {
+        if (val.employeeType === 'engineer') {
+            engineerInput();
+        } else if (val.employeeType === 'intern') {
+            internInput();
+        } else {
+            generateHTML();
+        }
+    });
+}
+
+// Gather engineer iformation if user opted to add 
